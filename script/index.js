e4fb4751468c7367b25c7b62d1e2577e2a29cb5c
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-08-10 23:23:09 +0600

    active class added in buttons and remove active class

--- script/index.js
+++ script/index.js
@@ -1,12 +1,21 @@
 
-const loadCategories = ()=> {
-    //1- fetch to data
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+function removeActiveClass() {
+  const activeButtons = document.getElementsByClassName('active');
+  for (let btn of activeButtons) {
+    btn.classList.remove('active')
+  }
+  console.log(activeButtons);
+}
+
+
+const loadCategories = () => {
+  //1- fetch to data
+  fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     //2- convert promise to json 
-    .then(res=> res.json())
+    .then(res => res.json())
     // 3- send data to display
-    .then(data=>{
-        displayCategories(data.categories)
+    .then(data => {
+      displayCategories(data.categories)
     })
 }
 // {
@@ -28,53 +37,60 @@ const loadCategories = ()=> {
 //   "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
 // }
 
-const loadVideos = ()=> {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res=> res.json())
-    .then(data=>displayVideos(data.videos))
+const loadVideos = () => {
+  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data =>{
+      removeActiveClass();
+      document.getElementById('btn-all').classList.add("active");
+     displayVideos(data.videos);
+    });
 }
 
-const loadCategoryVideos = (id)=>{
- 
-  const url=`https://openapi.programming-hero.com/api/phero-tube/category/${id} `;
+const loadCategoryVideos = (id) => {
+
+
+
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id} `;
   console.log(url);
 
   fetch(url)
-  .then((res)=> res.json())
-  .then((data)=>{
-    const clickedButton = document.getElementById(`btn-${id}`);
-    clickedButton.classList.add('active');
-    console.log(clickedButton);
-    displayVideos(data.category)
-  });
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveClass()
+      const clickedButton = document.getElementById(`btn-${id}`);
+      clickedButton.classList.add('active');
+      // console.log(clickedButton);
+      displayVideos(data.category)
+    });
 
 };
 
-const displayCategories =(categories)=>{
-   //   get the container
-   const categoryContainer = document.getElementById('category-container');
+const displayCategories = (categories) => {
+  //   get the container
+  const categoryContainer = document.getElementById('category-container');
 
-    // Loop operation on Array of object
-    for(let cat of categories){
-        // console.log(cat)
-          //    create Element
-          const categoryDiv =document.createElement('div');
-          categoryDiv.innerHTML=`
+  // Loop operation on Array of object
+  for (let cat of categories) {
+    // console.log(cat)
+    //    create Element
+    const categoryDiv = document.createElement('div');
+    categoryDiv.innerHTML = `
            <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
           `
-          // Append the Element
-          categoryContainer.append(categoryDiv);
-    }
-      
+    // Append the Element
+    categoryContainer.append(categoryDiv);
+  }
+
 }
 
-const displayVideos =(videos)=>{
-    const videoContainer = document.getElementById('video-container');
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById('video-container');
 
-    videoContainer.innerHTML ="";
+  videoContainer.innerHTML = "";
 
-    if (videos.length == 0) {
-      videoContainer.innerHTML = `
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
       <div
           class="py-20 col-span-full flex flex-col justify-center items-center text-center"
         >
@@ -84,12 +100,12 @@ const displayVideos =(videos)=>{
           </h2>
         </div>
       `;
-      
-      return;
-    }
+
+    return;
+  }
 
   videos.forEach((video) => {
-    console.log(video);
+    // console.log(video);
 
     const videoCard = document.createElement("div");
 
@@ -120,7 +136,7 @@ const displayVideos =(videos)=>{
     `
     videoContainer.append(videoCard)
   });
-     
+
 };
 
 loadCategories()
--- script/index2.js
+++ script/index2.js
@@ -44,8 +44,8 @@ const loadCategoryVideos = (id)=>{
   .then((res)=> res.json())
   .then((data)=>{
     const clickedButton = document.getElementById(`btn-${id}`);
-    // clickedButton.classList.add('active');
-    // console.log(clickedButton);
+    clickedButton.classList.add('active');
+    console.log(clickedButton);
     displayVideos(data.category)
   });
 
@@ -58,14 +58,32 @@ const categoryContainer = document.getElementById('category-container')
         
         const categoryDiv = document.createElement('div')
         categoryDiv.innerHTML=`
-        <button onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        <button id="btn-${cat.category_id}"onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         ` 
         categoryContainer.appendChild(categoryDiv)
     }
 }
 const displayVideos = (videos) =>{
     const videoContainer = document.getElementById('video-container');
+
     videoContainer.innerHTML ="";
+
+      if (videos.length == 0) {
+      videoContainer.innerHTML = `
+      <div
+          class="py-20 col-span-full flex flex-col justify-center items-center text-center"
+        >
+          <img class="w-[120px]" src="./assets/Icon.png" alt="" />
+          <h2 class="text-2xl font-bold">
+            Oops!! Sorry, There is no content here
+          </h2>
+        </div>
+      `;
+      
+      return;
+    }
+
+
     for(const video of videos){
         const videoCard = document.createElement('div')
         videoCard.innerHTML = `
